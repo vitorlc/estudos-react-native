546af50d391b82b69e2689eadf2816dc2e03ae6a
vitorlc <vitor_cirqueira1@hotmail.com>
2020-05-24 11:58:10 -0300

    add exercise multi components

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -7,6 +7,7 @@ import {
 
 import Simples from './components/Simples'
 import ParImpar from './components/ParImpar'
+import {Inverter, MegaSena} from './components/Multi'
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
       <Simples texto= "Texto por props"/>
       <ParImpar numero={3}/>
       <ParImpar numero={2}/>
+      <Inverter texto="React Native"/>
+      <MegaSena numeros={10}/>
     </View>
   );
 };
